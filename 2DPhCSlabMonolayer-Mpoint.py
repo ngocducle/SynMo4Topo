da211414996070942e43cca2da57464f2c680cf7
Le Ngoc Duc <62013383+ngocducle@users.noreply.github.com>
2024-02-05 08:43:24 +0100

    Add files via upload

--- 2DPhCSlabMonolayer-Mpoint.py
+++ 2DPhCSlabMonolayer-Mpoint.py
@@ -37,27 +37,37 @@ print('W = '+str(W))
 Nk = 50 # number of momenta 
 Kmax = 0.05 # maximum value of q from M-point 
 
-qxarray = np.concatenate( (np.linspace(-Kmax, 0, Nk), # Gamma' -> X'
-                           np.linspace(0, 0, Nk),    # X' -> M 
-                           np.linspace(0, -Kmax, Nk)  # M -> Gamma'  
-                          ),
-                          axis = 0 ) 
+#qxarray = np.concatenate( (np.linspace(-Kmax, 0, Nk), # Gamma' -> X'
+#                           np.linspace(0, 0, Nk),    # X' -> M 
+#                           np.linspace(0, -Kmax, Nk)  # M -> Gamma'  
+#                          ),
+#                          axis = 0 ) 
 
-qyarray = np.concatenate( (np.linspace(-Kmax,-Kmax,Nk), # Gamma' -> X'
-                           np.linspace(-Kmax, 0, Nk),  # X' -> M  
-                           np.linspace(0, -Kmax, Nk)   # M -> Gamma'  
-                          ), 
-                          axis = 0 )  
+#qyarray = np.concatenate( (np.linspace(-Kmax,-Kmax,Nk), # Gamma' -> X'
+#                           np.linspace(-Kmax, 0, Nk),  # X' -> M  
+#                           np.linspace(0, -Kmax, Nk)   # M -> Gamma'  
+#                          ), 
+#                          axis = 0 )  
+
+qxarray1 = np.linspace(-Kmax,0,Nk)  # Gamma' -> X' 
+qxarray2 = np.linspace(0,0,Nk)      # X' -> M 
+qxarray3 = np.linspace(0,-Kmax,Nk)  # M -> Gamma 
+qxarray = np.concatenate( (qxarray1, qxarray2[1:Nk], qxarray3[1:Nk]), axis = 0 ) 
+
+qyarray1 = np.linspace(-Kmax,-Kmax,Nk) # Gamma' -> X' 
+qyarray2 = np.linspace(-Kmax,0,Nk)     # X' -> M 
+qyarray3 = np.linspace(0,-Kmax,Nk)     # M -> Gamma' 
+qyarray = np.concatenate( (qyarray1, qyarray2[1:Nk], qyarray3[1:Nk]), axis = 0 ) 
 
 # Diagonalize the Hamiltonian 
-E1 = np.empty(3*Nk, dtype = complex) 
-E2 = np.empty(3*Nk, dtype = complex)
-E3 = np.empty(3*Nk, dtype = complex)
-E4 = np.empty(3*Nk, dtype = complex)  
+E1 = np.empty(3*Nk-2, dtype = complex) 
+E2 = np.empty(3*Nk-2, dtype = complex)
+E3 = np.empty(3*Nk-2, dtype = complex)
+E4 = np.empty(3*Nk-2, dtype = complex)  
 
-index = np.arange(3*Nk) 
+index = np.arange(3*Nk-2) 
 
-for i in range(3*Nk): 
+for i in range(3*Nk-2): 
     qx = qxarray[i]  
     qy = qyarray[i]
 
@@ -75,9 +85,9 @@ plt.plot(index, E2)
 plt.plot(index, E3)
 plt.plot(index, E4) 
 plt.vlines(Nk, 0, 1, linestyle = 'dashed', color = 'black') 
-plt.vlines(2*Nk,0,1, linestyle = 'dashed', color = 'black') 
+plt.vlines(2*Nk-1,0,1, linestyle = 'dashed', color = 'black') 
 plt.ylim(0,0.8) 
-tick_locs = [0, Nk, 2*Nk, 3*Nk] 
+tick_locs = [0, Nk, 2*Nk-1, 3*Nk-2]  
 tick_labs = [ r'$\Gamma \prime$', r'$X\prime$', 'M', r'$\Gamma \prime$' ]
 ax.set_xticks(tick_locs) 
 ax.set_xticklabels(tick_labs, size = 16) 
