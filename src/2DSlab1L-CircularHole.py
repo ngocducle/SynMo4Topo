4fcfd2bd4c639c5f5426625c73633fda29295dd4
investigator <investigator@U039932.sw>
2024-04-04 10:48:11 +0200

    Edit 2DSlab1L-CircularHole.py

--- src/2DSlab1L-CircularHole.py
+++ src/2DSlab1L-CircularHole.py
@@ -3,3 +3,17 @@ import meep as mp
 from meep import mpb
 import matplotlib.pyplot as plt
 
+# Resolution 
+resolution = mp.Vector3(16,16,16)   # pixels/a
+
+# Geometrical parameters
+h = 0.40     # Thickness of the slab
+radius = 0.4 # Radius of the hole
+Lz = 5.0     # The height of the unit cell along the z-direction
+
+# Number of bands
+num_bands = 18 
+
+# Number of k-points to interpolate between 2 high-symmetry points
+Nk = 19 
+
