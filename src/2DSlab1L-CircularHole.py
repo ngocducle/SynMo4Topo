903c32539970c41103e9640f61d792fada90c5f9
investigator <investigator@U039932.sw>
2024-04-04 14:54:50 +0200

    Make plotting the band structure a function

--- src/2DSlab1L-CircularHole.py
+++ src/2DSlab1L-CircularHole.py
@@ -108,18 +108,23 @@ number = np.arange(len(k_points))
 namesave = '2DSlab1L-CircularHole-h_'+str(h)+'-r_'+str(radius)+'-'+mode 
 print(namesave)
 
-### Plot the bands 
-fig, ax = plt.subplots()
-ax.plot(number, freqs)
-plt.vlines(Nk+1,0.0,1.0,linestyle='dashed',color='black')
-plt.vlines(2*(Nk+1),0.0,1.0,linestyle='dashed',color='black')
-plt.xlim(0,3*(Nk+1))
-plt.ylim(0,0.5)
-tick_locs = [i*(Nk+1) for i in range(4)]
-tick_labs = [r'$\Gamma$','X','M',r'$\Gamma$']
-ax.set_xticks(tick_locs)
-ax.set_xticklabels(tick_labs,size=16)
-ax.set_ylabel(r'$\omega a / (2 \pi c)$', fontsize = 14)
-plt.title(namesave,fontsize=14)
-plt.savefig(namesave+'.png')
-plt.show()
+### The function to plot the band structure:  
+def PlotBand_BrillouinZone(number,freqs,namesave):
+    fig, ax = plt.subplots()
+    ax.plot(number, freqs)
+    plt.vlines(Nk+1,0.0,1.0,linestyle='dashed',color='black')
+    plt.vlines(2*(Nk+1),0.0,1.0,linestyle='dashed',color='black')
+    plt.xlim(0,3*(Nk+1))
+    plt.ylim(0,0.5)
+    tick_locs = [i*(Nk+1) for i in range(4)]
+    tick_labs = [r'$\Gamma$','X','M',r'$\Gamma$']
+    ax.set_xticks(tick_locs)
+    ax.set_xticklabels(tick_labs,size=16)
+    ax.set_ylabel(r'$\omega a / (2 \pi c)$', fontsize = 14)
+    plt.title(namesave,fontsize=14)
+    plt.savefig(namesave+'.png')
+    plt.show()
+
+
+### Plot the band structure
+PlotBand_BrillouinZone(number,freqs,namesave)
