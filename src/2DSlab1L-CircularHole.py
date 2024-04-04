efd9447796f72ad279edf970ca53580d32ca892a
investigator <investigator@U039932.sw>
2024-04-04 14:48:55 +0200

    Plot the bands

--- src/2DSlab1L-CircularHole.py
+++ src/2DSlab1L-CircularHole.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 ### The mode of the simulation, corresponding to the polarization
 # Choose between: 'all', 'zeven', 'zodd' 
-mode = 'zodd'
+mode = 'all'
 
 ### Resolution 
-resolution = mp.Vector3(16,16,16)   # pixels/a
+resolution = mp.Vector3(8,8,8)   # pixels/a
 
 ### Geometrical parameters
 h = 0.40     # Thickness of the slab
@@ -98,11 +98,28 @@ else:
     print('ERROR! The mode does not belong to the allowed list')
     exit()
 
+# Extract the frequencies of the modes from the ModeSolver
 freqs = ms.all_freqs
 
+# The number of elements in k_points
 number = np.arange(len(k_points))
+
+# The title and the name of the files 
+namesave = '2DSlab1L-CircularHole-h_'+str(h)+'-r_'+str(radius)+'-'+mode 
+print(namesave)
 
 ### Plot the bands 
 fig, ax = plt.subplots()
 ax.plot(number, freqs)
+plt.vlines(Nk+1,0.0,1.0,linestyle='dashed',color='black')
+plt.vlines(2*(Nk+1),0.0,1.0,linestyle='dashed',color='black')
+plt.xlim(0,3*(Nk+1))
+plt.ylim(0,0.5)
+tick_locs = [i*(Nk+1) for i in range(4)]
+tick_labs = [r'$\Gamma$','X','M',r'$\Gamma$']
+ax.set_xticks(tick_locs)
+ax.set_xticklabels(tick_labs,size=16)
+ax.set_ylabel(r'$\omega a / (2 \pi c)$', fontsize = 14)
+plt.title(namesave,fontsize=14)
+plt.savefig(namesave+'.png')
 plt.show()
