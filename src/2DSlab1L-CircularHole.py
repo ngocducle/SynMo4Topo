7388aad4bf54a47e8b99f8e67dba9e8837db1479
investigator <investigator@U039932.sw>
2024-04-04 16:02:18 +0200

    Add the module ModeSolvers

--- src/2DSlab1L-CircularHole.py
+++ src/2DSlab1L-CircularHole.py
@@ -1,7 +1,7 @@
 import numpy as np
 import meep as mp 
 from meep import mpb
-import matplotlib.pyplot as plt
+from ModeSolvers import _2DSlab1LCircularHole
 from PlotBandStructure import PlotBand_BrillouinZone
 
 ### The light polarization
@@ -26,81 +26,6 @@ num_bands = 8
 ### Number of k-points to interpolate between 2 high-symmetry points
 Nk = 9 
 
-##### FUNCTION: ModeSolver 2D photonic crystal slab monolayer with circular holes
-def _2DSlab1LCircularHole(h,Lz,radius,num_bands,Nk,resolution,kSpace):
-    ### Define the k-points
-    if kSpace == 'BZ':  # The whole Brillouin zone
-        k_points = [
-            mp.Vector3(0.0,0.0,0.0),    # Gamma
-            mp.Vector3(0.5,0.0,0.0),    # X
-            mp.Vector3(0.5,0.5,0.0),    # M 
-            mp.Vector3(0.0,0.0,0.0)     # Gamma 
-        ]
-    elif kSpace == 'M': # In the vicinity of the M-point
-        k_points = [
-            mp.Vector3(0.3,0.3,0.0),    # Gamma'
-            mp.Vector3(0.5,0.3,0.0),    # X
-            mp.Vector3(0.5,0.5,0.0),    # M 
-            mp.Vector3(0.3,0.3,0.0)     # Gamma 
-        ]
-    else:
-        print('ERROR! The k-point has not been in the allowed list yet')
-        exit()
-
-    # Interpolate to get the points on the lines
-    k_points = mp.interpolate(Nk,k_points)
-
-    ### Define the materials
-    Si = mp.Medium(index = 3.54)
-    SiO2 = mp.Medium(index = 1.46)
-    Dielectric = mp.Medium(epsilon = 12.0)
-    Air = mp.Medium(epsilon = 1.0)
-
-    Environment = Air 
-
-    ### Define the lattice
-    geometry_lattice = mp.Lattice(
-        size = mp.Vector3(1.0,1.0,Lz),
-        basis1 = mp.Vector3(1.0,0.0),
-        basis2 = mp.Vector3(0.0,1.0)
-    )
-
-    ### Define the geometry
-    geometry = [
-        mp.Block(
-            center = mp.Vector3(0.0,0.0,0.0),
-            size = mp.Vector3(mp.inf,mp.inf,mp.inf),
-            material = Environment
-        ),
-
-        mp.Block(
-            center = mp.Vector3(0.0,0.0,0.0),
-            size = mp.Vector3(1.0,1.0,h),
-            material = Si
-        ),
-
-        mp.Cylinder(
-            center = mp.Vector3(0.0,0.0,0.0),
-            radius = radius,
-            height = h,
-            axis = mp.Vector3(0,0,1),
-            material = Environment
-        )
-    ]
-
-    ### The ModeSolver
-    ModeSolver = mpb.ModeSolver(
-        geometry = geometry,
-        geometry_lattice = geometry_lattice,
-        k_points = k_points,
-        resolution = resolution,
-        num_bands = num_bands
-    )
-
-    ### Return the mode solver
-    return ModeSolver
-
-##### The MAIN program continues here
 ### Define the mode solver
 ms = _2DSlab1LCircularHole(h,Lz,radius,num_bands,Nk,resolution,kSpace)
 
---
+++ src/ModeSolvers.py
@@ -0,0 +1,83 @@
+import meep as mp
+from meep import mpb 
+
+### ================================================================================ ###
+###                                                                                  ###
+###     This module contains the functions to define the mode solver in              ### 
+###     different structures, with specified materials                               ###
+###                                                                                  ###
+### ================================================================================ ###
+
+##### FUNCTION: ModeSolver 2D photonic crystal slab monolayer with circular holes
+def _2DSlab1LCircularHole(h,Lz,radius,num_bands,Nk,resolution,kSpace):
+    ### Define the k-points
+    if kSpace == 'BZ':  # The whole Brillouin zone
+        k_points = [
+            mp.Vector3(0.0,0.0,0.0),    # Gamma
+            mp.Vector3(0.5,0.0,0.0),    # X
+            mp.Vector3(0.5,0.5,0.0),    # M 
+            mp.Vector3(0.0,0.0,0.0)     # Gamma 
+        ]
+    elif kSpace == 'M': # In the vicinity of the M-point
+        k_points = [
+            mp.Vector3(0.3,0.3,0.0),    # Gamma'
+            mp.Vector3(0.5,0.3,0.0),    # X
+            mp.Vector3(0.5,0.5,0.0),    # M 
+            mp.Vector3(0.3,0.3,0.0)     # Gamma 
+        ]
+    else:
+        print('ERROR! The k-point has not been in the allowed list yet')
+        exit()
+
+    # Interpolate to get the points on the lines
+    k_points = mp.interpolate(Nk,k_points)
+
+    ### Define the materials
+    Si = mp.Medium(index = 3.54)
+    SiO2 = mp.Medium(index = 1.46)
+    Dielectric = mp.Medium(epsilon = 12.0)
+    Air = mp.Medium(epsilon = 1.0)
+
+    Environment = Air 
+
+    ### Define the lattice
+    geometry_lattice = mp.Lattice(
+        size = mp.Vector3(1.0,1.0,Lz),
+        basis1 = mp.Vector3(1.0,0.0),
+        basis2 = mp.Vector3(0.0,1.0)
+    )
+
+    ### Define the geometry
+    geometry = [
+        mp.Block(
+            center = mp.Vector3(0.0,0.0,0.0),
+            size = mp.Vector3(mp.inf,mp.inf,mp.inf),
+            material = Environment
+        ),
+
+        mp.Block(
+            center = mp.Vector3(0.0,0.0,0.0),
+            size = mp.Vector3(1.0,1.0,h),
+            material = Si
+        ),
+
+        mp.Cylinder(
+            center = mp.Vector3(0.0,0.0,0.0),
+            radius = radius,
+            height = h,
+            axis = mp.Vector3(0,0,1),
+            material = Environment
+        )
+    ]
+
+    ### The ModeSolver
+    ModeSolver = mpb.ModeSolver(
+        geometry = geometry,
+        geometry_lattice = geometry_lattice,
+        k_points = k_points,
+        resolution = resolution,
+        num_bands = num_bands
+    )
+
+    ### Return the mode solver
+    return ModeSolver
