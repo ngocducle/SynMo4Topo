7ab0896a68848518b4ed989eb9e839be6475a563
investigator <investigator@U039932.sw>
2024-04-04 16:53:04 +0200

    Arrange all in the main program

--- Programs/2DSlab1L-CircularHole.py
+++ Programs/2DSlab1L-CircularHole.py
@@ -8,60 +8,67 @@ from ModeSolvers import _2DSlab1LCircularHole
 from PlotBandStructure import *
 from ExportData import PrintBandStructure
 
-### The light polarization
-# Choose between: 'all', 'zeven', 'zodd' 
-polarization = 'all'
 
-### The part of the momentum space to plot the band structure
-# Choose between: 'BZ', 'M' 
-kSpace = 'BZ'
+##### The MAIN program goes here 
+def main():
+    ### The light polarization
+    # Choose between: 'all', 'zeven', 'zodd' 
+    polarization = 'all'
 
-### Resolution 
-resolution = mp.Vector3(8,8,8)   # pixels/a
+    ### The part of the momentum space to plot the band structure
+    # Choose between: 'BZ', 'M' 
+    kSpace = 'BZ'
 
-### Geometrical parameters
-h = 0.40     # Thickness of the slab
-radius = 0.4 # Radius of the hole
-Lz = 5.0     # The height of the unit cell along the z-direction
+    ### Resolution 
+    resolution = mp.Vector3(8,8,8)   # pixels/a
 
-### Number of bands
-num_bands = 8 
+    ### Geometrical parameters
+    h = 0.40     # Thickness of the slab
+    radius = 0.4 # Radius of the hole
+    Lz = 5.0     # The height of the unit cell along the z-direction
 
-### Number of k-points to interpolate between 2 high-symmetry points
-Nk = 9 
+    ### Number of bands
+    num_bands = 8 
 
-### Define the mode solver
-ms = _2DSlab1LCircularHole(h,Lz,radius,num_bands,Nk,resolution,kSpace)
+    ### Number of k-points to interpolate between 2 high-symmetry points
+    Nk = 9 
 
-### Run the simulation
-if polarization == 'all':
-    ms.run()
-elif polarization == 'zeven':
-    ms.run_zeven()
-elif polarization == 'zodd':
-    ms.run_zodd()
-else:
-    print('ERROR! The polarization does not belong to the allowed list')
-    exit()
+    ### Define the mode solver
+    ms = _2DSlab1LCircularHole(h,Lz,radius,num_bands,Nk,resolution,kSpace)
 
-### Extract the frequencies of the modes from the ModeSolver
-freqs = ms.all_freqs
+    ### Run the simulation
+    if polarization == 'all':
+        ms.run()
+    elif polarization == 'zeven':
+        ms.run_zeven()
+    elif polarization == 'zodd':
+        ms.run_zodd()
+    else:
+        print('ERROR! The polarization does not belong to the allowed list')
+        exit()
 
-### The number of elements in k_points
-number = np.arange(len(ms.k_points))
+    ### Extract the frequencies of the modes from the ModeSolver
+    freqs = ms.all_freqs
 
-### The title and the name of the files 
-namesave = '2DSlab1L-CircularHole-h_'+str(h)+'-r_'+str(radius) \
-    +'-'+polarization+'-'+kSpace 
+    ### The number of elements in k_points
+    number = np.arange(len(ms.k_points))
 
-### Print the band structure to file
-PrintBandStructure(freqs,number,namesave)
+    ### The title and the name of the files 
+    namesave = '2DSlab1L-CircularHole-h_'+str(h)+'-r_'+str(radius) \
+        +'-'+polarization+'-'+kSpace 
 
-### Plot the band structure
-if kSpace == 'BZ':
-    PlotBand_BrillouinZone(number,freqs,Nk,namesave)
-elif kSpace == 'M':
-    PlotBand_M(number,freqs,Nk,namesave)
-else:
-    print('ERROR! The k-point has not been in the allowed list yet')
-    exit()
+    ### Print the band structure to file
+    PrintBandStructure(freqs,number,namesave)
+
+    ### Plot the band structure
+    if kSpace == 'BZ':
+        PlotBand_BrillouinZone(number,freqs,Nk,namesave)
+    elif kSpace == 'M':
+        PlotBand_M(number,freqs,Nk,namesave)
+    else:
+        print('ERROR! The k-point has not been in the allowed list yet')
+        exit()
+
+##### Run the MAIN program
+if __name__ == "__main__":
+    main()
