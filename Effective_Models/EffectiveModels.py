c77bdcc24a29522c172d684c0b10791847f9f70d
investigator <investigator@U039932.sw>
2024-04-11 08:11:44 +0200

    Module EffectiveModels: add function EffModel_2DSlab2L_M

--- Effective_Models/EffectiveModels.py
+++ Effective_Models/EffectiveModels.py
@@ -24,3 +24,33 @@ def EffModel_2DSlab1L_M(qx,qy,omegaM,v,U,W):
 
     return evalues, evectors
 
+### FUNCTION: Hamiltonian of 2D photonic crystal slab monolayer at M-point
+def EffModel_2DSlab2L_M(qx,qy,deltax,deltay,omegaM,v,U,W,V):
+    omegapp = omegaM + v*(qx+qy)/np.sqrt(2) 
+    omegapm = omegaM + v*(qx-qy)/np.sqrt(2)
+    omegamp = omegaM + v*(-qx+qy)/np.sqrt(2)
+    omegamm = omegaM + v*(-qx-qy)/np.sqrt(2)
+
+    K = 2.0*np.pi
+
+    Vpp = V * cmath.exp(- 0.5 * 1j * (qx+qy) * K)
+    Vpm = V * cmath.exp(- 0.5 * 1j * (qx-qy) * K)
+    Vmp = V * cmath.exp(- 0.5 * 1j * (-qx+qy) * K)
+    Vmm = V * cmath.exp(- 0.5 * 1j * (-qx-qy) * K)
+
+    Hamiltonian = np.array(
+        [
+            [omegapp, W, W, U, Vpp, 0, 0, 0],
+        [W, omegapm, U, W, 0, Vpm, 0, 0],
+        [W, U, omegamp, W, 0, 0, Vmp, 0],
+        [U, W, W, omegamm, 0, 0, 0, Vmm],
+        [Vmm, 0, 0, 0, omegapp, W, W, U],
+        [0, Vmp, 0, 0, W, omegapm, U, W],
+        [0, 0, Vpm, 0, W, U, omegamp, W],
+        [0, 0, 0, Vpp, U, W, W, omegamm]
+        ]
+    )
+
+    evalues, evectors = sla.eigh(Hamiltonian)
+
+    return evalues, evectors 
