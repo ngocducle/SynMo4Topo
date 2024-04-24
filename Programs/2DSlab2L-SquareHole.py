52e9ab039926c3cf99f85b719febd4c912af3830
investigator <investigator@U039932.sw>
2024-04-24 08:28:27 +0200

    Add program 2DSlab2L-SquareHole

--- Programs/2DSlab2L-SquareHole.py
+++ Programs/2DSlab2L-SquareHole.py
@@ -4,13 +4,13 @@ from meep import mpb
 
 import sys
 sys.path.insert(0,'../src/')
-from ModeSolvers import _2DSlab1LSquareHole
+from ModeSolvers import _2DSlab2LSquareHole
 from DielectricProfile import *
 from BandStructure import *
 from ExportData import *
 from EFieldProfile import *
 from HFieldProfile import *
-from Materials import * 
+from Materials import *
 
 
 ##### The MAIN program goes here 
@@ -24,11 +24,11 @@ def main():
 
     ### The light polarization
     # Choose between: 'all', 'zeven', 'zodd' 
-    polarization = 'zodd'
+    polarization = 'all'
     print('# Polarization = '+polarization)
 
     ### The part of the momentum space to plot the band structure
-    # Choose between: 'BZ', 'M-vicinity' 
+    # Choose between: 'BZ', 'M', 'M-vicinity' 
     kSpace = 'BZ'
     print('# The part of the momentum space to simulate:'+kSpace)
 
@@ -41,20 +41,26 @@ def main():
     print('# The resolution:'+str(resolution))
 
     ### Geometrical parameters
-    h = 0.4      # Thickness of the slab
-    edge = 0.45  # Edge of the square hole
-    Lz = 5.0     # The height of the unit cell along the z-direction
+    h = 0.3      # Thickness of the slab
+    edge = 0.6   # Edge of the square hole
+    dist = 0.05  # Distance between the two layers 
+    delta1 = 0.0 # Relative displacement along the x-direction
+    delta2 = 0.0 # Relative displacement along the y-direction
+    Lz = 10.0    # The height of the unit cell along the z-direction
 
     print('# Thickness of the slab h = '+str(h))
     print('# Edge of the square hole b = '+str(edge))
     print('# The height of the simulation cell Lz = '+str(Lz))
+    print('# Relative shift along x-direction delta1 = '+str(delta1))
+    print('# Relative shift along y-direction delta2 = '+str(delta2))
+    print('# Interlayer distance dist = '+str(dist))
 
     ### Number of bands
-    num_bands = 20
+    num_bands = 25
     print('# The number of bands to simulate: '+str(num_bands))
 
     ### Number of k-points to interpolate between 2 high-symmetry points
-    Nk = 29 
+    Nk = 29  
     print('# The number of points to interpolate the high-symmetry line Nk = '+str(Nk))
 
     ### Show figure (Yes/No)
@@ -75,8 +81,8 @@ def main():
     resolution_field = 81  # Number of pixels per a 
     num_periods = 3        # Number of periods along each direction  
 
-    # The value of z where we take a slice to plot the field
-    zvalue = 0.075 
+    # The value of z where we take a slice to plot the fields 
+    zvalue = 0.5*(dist+h)
 
     ##########################################################################
     #                                                                        #
@@ -85,7 +91,8 @@ def main():
     ##########################################################################
 
     ### Define the mode solver
-    ms = _2DSlab1LSquareHole(h,Lz,edge,num_bands,Nk,resolution,kSpace,Mater,Envir)
+    ms = _2DSlab2LSquareHole(h,Lz,edge,dist,delta1,delta2,
+                          num_bands,Nk,resolution,kSpace,Mater,Envir)
 
     ### Run the simulation
     if polarization == 'all':
@@ -105,14 +112,14 @@ def main():
     number = np.arange(len(ms.k_points))
 
     ### The title and the name of the files 
-    namesave = '2DSlab1L-SquareHole-h_'+str(h)+'-b_'+str(edge) \
+    namesave = '2DSlab2L-SquareHole-h_'+str(h)+'-b_'+str(edge) \
         +'-'+polarization+'-'+kSpace 
     
 
 
     ### Output the dielectric profile along the z-direction with projected (X,Y)
-    x = 0.499
-    y = 0.499
+    x = 0.50 
+    y = 0.50 
     zmin = -0.5*Lz
     zmax = 0.5*Lz 
     Nz = 50 
@@ -134,11 +141,11 @@ def main():
     ### for zmin <= z <= zmax, with Nz values of z 
     Ncellx = 5
     Ncelly = 5
-    zmin = -0.2*Lz
-    zmax = 0.2*Lz 
+    zmin = -0.5*dist-h 
+    zmax = 0.5*dist+h  
     Nx = 300
     Ny = 300
-    Nz = 9
+    Nz = 13
 
     # Calculate the dielectric profile in planes parallel to Oxy
     x_plot,y_plot,z_array,epsilon_xy_array \
@@ -166,11 +173,12 @@ def main():
     else:
         print('ERROR! The k-point has not been in the allowed list yet')
         exit()
-    
+
     """
     # Define the mode solver to calculate the E-field
-    Efieldx,Efieldy,Efieldz,X,Y,Xfield,Yfield,eps_Oxy = EFields_2DSlab1LCircularHole(h,Lz,
-                                radius,num_bands,resolution,Mater,Envir,
+    Efieldx,Efieldy,Efieldz,X,Y,Xfield,Yfield,eps_Oxy = EFields_2DSlab2LCircularHole(h,Lz,
+                                radius,dist,delta1,delta2,num_bands,resolution,
+                                Mater,Envir,
                                 k_field,zvalue,
                                 polarization,resolution_eps,resolution_field,
                                 num_periods,Bloch_Phase)
@@ -180,8 +188,9 @@ def main():
                        X,Y,eps_Oxy,Xfield,Yfield,num_periods,show_fig)
     
     # Define the mode solver to calculate the H-field
-    Hfieldx,Hfieldy,Hfieldz,X,Y,Xfield,Yfield,eps_Oxy = HFields_2DSlab1LCircularHole(h,Lz,
-                                radius,num_bands,resolution,Mater,Envir,
+    Hfieldx,Hfieldy,Hfieldz,X,Y,Xfield,Yfield,eps_Oxy = HFields_2DSlab2LCircularHole(h,Lz,
+                                radius,dist,delta1,delta2,num_bands,resolution,
+                                Mater,Envir,
                                 k_field,zvalue,
                                 polarization,resolution_eps,resolution_field,
                                 num_periods,Bloch_Phase)
