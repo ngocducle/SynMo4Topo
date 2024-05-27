774d0af1535eae1be821a88d482582c0ea66ff17
investigator <investigator@U039932.sw>
2024-05-27 11:19:19 +0200

    Create the directory Structures

---
+++ Structures/Diagonal_symmetry_breaking_unit_cell.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import scipy 
+import matplotlib.pyplot as plt 
+
+### ==================================================================
+###
+### Plot the unit cell for the case of diagonal symmetry-breaking 
+###
+### ================================================================== 
+
