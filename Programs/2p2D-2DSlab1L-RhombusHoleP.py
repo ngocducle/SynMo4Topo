b0f6b64e7348da63b561a31ee665b27aa9a2f00a
investigator <investigator@U039932.sw>
2024-07-04 11:07:13 +0200

    2p2D-2DSlab1L-RhombusHoleP: define the geometrical parameters

--- Programs/2p2D-2DSlab1L-RhombusHoleP.py
+++ Programs/2p2D-2DSlab1L-RhombusHoleP.py
@@ -23,6 +23,20 @@ def main():
     resolution = mp.Vector3(8,8,8)  # pixels/a 
     print('# The resolution:'+str(resolution))
 
+    ### Number of bands 
+    num_bands = 10 
+    print('# The number of bands to simulate: '+str(num_bands))
+    
+    ### Geometrical parameters 
+    h = 0.35        # Thickness of the slab 
+    Lz = 5.0        # The height of the unit cell along the z-direction 
+    b = 0.35        # The mean of the projections of the diagonal vertices (0 <= b < 0.5)
+    
+    print('# Thickness of the slab h = '+str(h))
+    print('# The edge of the undeformed square hole b = '+str(b))
+    print('# The height of the simulation cell Lz = '+str(Lz))
+
+    
 
 ##### Run the MAIN program 
 if __name__ == '__main__':
