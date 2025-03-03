676c279e61d2c12804db547838b8026acb01fd4a
ngocducle <lengocduc1009.lnd@gmail.com>
2025-03-03 16:13:59 +0100

    Goerbig-DiracCone-Scattering: add Berry curvature

--- Effective_Models/Goerbig-DiracCone-Scattering.py
+++ Effective_Models/Goerbig-DiracCone-Scattering.py
@@ -2,23 +2,29 @@ import numpy as np
 import scipy
 import scipy.linalg as sla  
 import matplotlib.pyplot as plt 
+from matplotlib import colors 
+from matplotlib.patches import Circle 
 
 ### The parameters 
 m = 2
-c = 0.1
-Delta = -0.1
+c = 0
+Delta = 0.0
+Mass = 0.0
 
 ### Array of momenta 
 Nx = 501 
-qx_array = np.linspace(-1,1,Nx)
+qx_array = np.linspace(-1.5,1.5,Nx)
 Ny = 501 
-qy_array = np.linspace(-1,1,Ny)
+qy_array = np.linspace(-1.5,1.5,Ny)
 
 ### The arrays of energy 
 E_array = np.zeros((Nx,Ny,2))
 
 ### The arrays of states
 States = np.zeros((Nx,Ny,2,2),dtype=complex)
+
+### The array of Berry curvature 
+F_array = np.zeros((Nx,Ny,2))
 
 ### We scan over qx and qy 
 for ix in range(Nx):
@@ -28,11 +34,23 @@ for ix in range(Nx):
 
         H = np.zeros((2,2),dtype=complex)
 
+        H[0,0] = Mass 
         H[0,1] = -(qx+1j*qy)*(qx+1j*qy)/(2*m) + c*(qx-1j*qy) + Delta 
         H[1,0] = -(qx-1j*qy)*(qx-1j*qy)/(2*m) + c*(qx+1j*qy) + np.conjugate(Delta)
+        H[1,1] = -Mass 
 
         #print('Hamiltonian = ')
         #print(H)
+
+        dHx = np.zeros((2,2),dtype=complex)
+
+        dHx[0,1] = -(qx+1j*qy)/m + c 
+        dHx[1,0] = -(qx-1j*qy)/m + c
+
+        dHy = np.zeros((2,2),dtype=complex)
+
+        dHy[0,1] = -1j*(qx+1j*qy)/m - 1j*c 
+        dHy[1,0] = 1j*(qx-1j*qy)/m + 1j*c 
 
         # Diagonalize the Hamiltonian
         evals,evecs = sla.eigh(H)
@@ -40,14 +58,45 @@ for ix in range(Nx):
         E_array[ix,iy,:] = evals 
         States[ix,iy,:,:] = evecs 
 
+        dHxe = np.matmul((evecs.conjugate()).transpose(),np.matmul(dHx,evecs))
+        dHye = np.matmul((evecs.conjugate()).transpose(),np.matmul(dHy,evecs))
+
+        for n in range(2):
+                for m in range(2):
+                    if (m != n):
+                        val = -2.0*np.imag(dHxe[n,m]*dHye[m,n]) / (evals[n]-evals[m])**2
+                        F_array[ix,iy,n] = F_array[ix,iy,n] + val
+
 ### Plot the band structure 
 X,Y = np.meshgrid(qx_array,qy_array)
 
-fig,ax = plt.subplots(subplot_kw={'projection':'3d'})
-ax.plot_surface(X,Y,E_array[:,:,0].T)
-ax.plot_surface(X,Y,E_array[:,:,1].T)
+ls = colors.LightSource(azdeg=225, altdeg=5, 
+                        hsv_min_val=0, hsv_max_val=1, 
+                        hsv_min_sat=1, hsv_max_sat=0)
+
+fcolors1 = ls.shade(E_array[:,:,0],cmap='binary',vert_exag=0.1,blend_mode='soft')
+fcolors2 = ls.shade(E_array[:,:,1],cmap='binary',vert_exag=0.1,blend_mode='soft')
+
+fig,ax = plt.subplots(subplot_kw={'projection':'3d'},figsize=(12,10))
+
+x_axis_x = np.linspace(-1.8,1.8,Nx)
+x_axis_y = np.linspace(0,0,Nx)
+x_axis_z = np.linspace(0,0,Nx)
+ax.plot(x_axis_x,x_axis_y,x_axis_z,color='black',linewidth=2)
+ax.text(1.5,0.25,-0.32,r'$k_x$',fontsize=24)
+
+y_axis_x = np.linspace(0,0,Ny)
+y_axis_y = np.linspace(-1.8,1.8,Ny)
+y_axis_z = np.linspace(0,0,Ny)
+ax.plot(y_axis_x,y_axis_y,y_axis_z,color='black',linewidth=2)
+ax.text(0,1.5,-0.4,r'$k_y$',fontsize=24)
+
+ax.plot_surface(X,Y,E_array[:,:,0].T,facecolor=fcolors1)
+ax.plot_surface(X,Y,E_array[:,:,1].T,facecolor=fcolors2)
 ax.set_xlabel('qx',fontsize=14)
 ax.set_ylabel('qy',fontsize=14)
+ax.view_init(elev=5, azim=30, roll=0)
+plt.axis('off')
 plt.show()
 
 ##### ========================================================================
@@ -114,5 +163,90 @@ for j in range(Ntheta):
 ax.set_xlabel('qx',fontsize=14)
 ax.set_ylabel('qy',fontsize=14)
 ax.set_title(f'Delta = {Delta:.4f}, w = {Berry_winding:.4f}',fontsize=14)
-plt.savefig('Berry_winding_qx_0.5.png')
+plt.savefig('Berry_winding.png')
+plt.show()
+
+##### ========================================================================
+###         Plot the dispersion surfaces with Berry curvature 
+##### ========================================================================
+##### For band 1 and band 2 only 
+F_array_3D = F_array 
+maxabs = abs(F_array_3D[:,:,0]).max()
+vmin, vmax = -maxabs,maxabs
+cmap = 'coolwarm'
+norm = colors.Normalize(vmin=vmin,vmax=vmax)
+scamap = plt.cm.ScalarMappable(norm=norm,cmap=cmap)
+linewidth = 0 
+
+fig,ax = plt.subplots(subplot_kw = {'projection':'3d'},
+                      figsize=(12,10))
+
+fcolors = scamap.to_rgba(F_array_3D[:,:,0].T)
+ax.plot_surface(X,Y,E_array[:,:,0].T,
+                linewidth=linewidth,
+                antialiased='False',
+                rstride=5,
+                cstride=5,
+                facecolors=fcolors,
+                cmap=cmap)
+
+fcolors = scamap.to_rgba(F_array_3D[:,:,1].T)
+ax.plot_surface(X,Y,E_array[:,:,1].T,
+                linewidth=linewidth,
+                antialiased='False',
+                rstride=5,
+                cstride=5,
+                facecolors=fcolors,
+                cmap=cmap)
+
+x_axis_x = np.linspace(-1.8,1.8,Nx)
+x_axis_y = np.linspace(0,0,Nx)
+x_axis_z = np.linspace(0,0,Nx)
+ax.plot(x_axis_x,x_axis_y,x_axis_z,color='black',linewidth=2)
+ax.text(1.5,0.25,-0.32,r'$k_x$',fontsize=24)
+
+y_axis_x = np.linspace(0,0,Ny)
+y_axis_y = np.linspace(-1.8,1.8,Ny)
+y_axis_z = np.linspace(0,0,Ny)
+ax.plot(y_axis_x,y_axis_y,y_axis_z,color='black',linewidth=2)
+ax.text(0,1.5,-0.4,r'$k_y$',fontsize=24)
+
+ax.set_xlabel(r'$q_x$',fontsize=14)
+ax.set_ylabel(r'$q_y$',fontsize=14)
+ax.set_zlabel(r'$\omega a / (2 \pi c)$',fontsize=14)
+"""fig.colorbar(scamap,
+             orientation='vertical',
+             shrink=0.4,
+             location = 'right',
+             ax=ax)"""
+ax.view_init(elev=5,azim=30,roll=0)
+plt.axis('off')
+plt.savefig('Dis-BCur.png')
+plt.show()
+plt.close()
+
+#####################################################################
+fig,ax = plt.subplots(subplot_kw = {'projection':'3d'},
+                      figsize=(12,10))
+
+x_axis_x = np.linspace(-1.8,1.8,Nx)
+x_axis_y = np.linspace(0,0,Nx)
+x_axis_z = np.linspace(0,0,Nx)
+ax.plot(x_axis_x,x_axis_y,x_axis_z,color='black',linewidth=2)
+ax.text(1.5,0.25,-0.32,r'$k_x$',fontsize=24)
+
+y_axis_x = np.linspace(0,0,Ny)
+y_axis_y = np.linspace(-1.8,1.8,Ny)
+y_axis_z = np.linspace(0,0,Ny)
+ax.plot(y_axis_x,y_axis_y,y_axis_z,color='black',linewidth=2)
+ax.text(0,1.5,-0.4,r'$k_y$',fontsize=24)
+
+z_axis_x = np.linspace(0,0,Nx) 
+z_axis_y = np.linspace(0,0,Nx)
+z_axis_z = np.linspace(-1.8,1.8,Nx)
+ax.plot(z_axis_x,z_axis_y,z_axis_z,color='black',linewidth=2)
+ax.text(0.2,0.2,1.5,r'$m$',fontsize=24)
+
+ax.view_init(elev=5,azim=30,roll=0)
+plt.axis('off')
 plt.show()
