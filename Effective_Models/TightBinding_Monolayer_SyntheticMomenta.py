8d79dbab9b74ed56d56f108027c18617171e37db
ephoni <lengocduc1009.lnd@gmail.com>
2026-01-05 17:17:07 +0100

    Initiate TightBinding_Monolayer_SyntheticMomenta.py

---
+++ Effective_Models/TightBinding_Monolayer_SyntheticMomenta.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot
+
+# Tight-binding Minh model for monolayer
+t1 = -0.81 # intracell hopping integrals along edge
+t2 = -0.74 # intercell hopping integrals
+t3 = -0.33 # intracell hopping integrals along diagonals
+a = 0.2    # lattice parameter (nm)
+
