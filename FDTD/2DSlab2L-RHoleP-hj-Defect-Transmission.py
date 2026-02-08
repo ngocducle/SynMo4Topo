e0bf69e76529100b5369a07de6d07e9e0a11d8d6
Le Ngoc Duc <62013383+ngocducle@users.noreply.github.com>
2026-02-08 11:01:41 +0100

    Update PML.Y, pulse width and reset MEEP simulation
    PML layers: add PML along y-direction.
    Increased pulse width from 0.010 to 0.020 and added reset functionality for MEEP simulation.

--- FDTD/2DSlab2L-RHoleP-hj-Defect-Transmission.py
+++ FDTD/2DSlab2L-RHoleP-hj-Defect-Transmission.py
@@ -17,6 +17,8 @@ resolution = 20
 dboundary = 3.0     # PML/Absorber thickness
 pml_layers = [mp.PML(direction = mp.X,
                      thickness = dboundary),
+              mp.PML(direction = mp.Y,
+                     thickness = dboundary),
               mp.PML(direction = mp.Z,
                      thickness = dboundary)
     ]
@@ -35,7 +37,7 @@ mix_layers = [mp.Absorber(direction = mp.X,
 
 ### The source
 fcen = 0.260 # pulse center frequency
-df = 0.010   # pulse width
+df = 0.020   # pulse width
 nfreq = 2001 # number of frequencies
 
 # The array of frequencies and wavelengths
@@ -247,4 +249,8 @@ for idelta in range(Ndelta):
         filename = namesave + '.txt'
         with open(filename,'w') as file:
             np.savetxt(file,datasave,'%.8f')
+
+    ##### ================================================================================
+    ##### Reset MEEP
+    sim.reset_meep()
 
